41ae4c99f1bb63ead8fb195ba0fe3f051bb19ea8
vamsi-krishna2580 <216156130+vamsi-krishna2580@users.noreply.github.com>
2026-03-05 06:16:01 +0530

    weather api-update

--- weather.py
+++ weather.py
@@ -1,10 +1,22 @@
 import requests
 
-API_KEY = "357cf98aa817c4ea8ce75a9f137ca7e3"
+API_KEY = "REMOVED_API_KEY"
 
+def get_coordinates(city):
+
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},IN&limit=1&appid={API_KEY}"
+
+    response = requests.get(url).json()
+
+    lat = response[0]["lat"]
+    lon = response[0]["lon"]
+
+    return lat, lon
 def get_weather(city):
 
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    lat, lon = get_coordinates(city)
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
 
     response = requests.get(url).json()
 
@@ -15,3 +27,4 @@ def get_weather(city):
     }
 
     return weather
+print(get_weather("Ongole"))
